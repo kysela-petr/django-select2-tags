c96189c15a3380e7e3063ef8d3b3aa9c6a4d0b46
jessamynsmith <jessamyn.smith@gmail.com>
2019-06-01 20:45:58 -0600

    Updated to circleci 2

--- select2_tags/__init__.py
+++ select2_tags/__init__.py
@@ -1,8 +1,5 @@
-import pkg_resources
-
-
 __title__ = 'django-select2-tags'
-__version__ = pkg_resources.get_distribution(__title__).version
+__version__ = '0.0.1'
 __author__ = 'Jessamyn Smith'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2017 %s' % __author__
